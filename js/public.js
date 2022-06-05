be566d6633cbb88634f32669bca7ff3362ee6c61
xxRache1 <ruiqix1@student.unimelb.edu.au>
2022-06-06 01:55:48 +0800

    responsive

--- js/public.js
+++ js/public.js
@@ -2,3 +2,16 @@ var clickpage = function(urlstr){
     var pathstr = urlstr;
     window.location.href= urlstr;
 }
+
+
+var isShow = true;
+var clickDetailsBtnRemo = function(){
+    if(isShow){
+        document.getElementsByClassName('navFlexbox')[0].classList.remove('hideBox');
+        isShow = false
+    }else{
+        document.getElementsByClassName('navFlexbox')[0].classList.add('hideBox');
+        isShow = true
+    }
+}
+
